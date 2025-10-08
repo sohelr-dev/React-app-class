60edef9554d9addac087ec94a5411f478e006bc0
sohelr-dev <sohe.4273@gmail.com>
2025-10-08 13:09:56 +0600

    Middleware , create  api token working with login to  access others  page

--- practice-project/src/config.tsx
+++ practice-project/src/config.tsx
@@ -7,7 +7,8 @@ const baseApiUrl ="http://localhost/php-react-api/api/";
 const api =axios.create({
      baseURL:baseApiUrl,
      headers:{
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoic29oZWwiLCJlbWFpbCI6InNvaGVsQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc1OTg5NjE1NiwiZXhwIjoxNzU5ODk5NzU2fQ.6Rhfd9j3_omtfJlZc6_UqArZpAuMIOfQxWSPSG2bTDo`
      }
 });
 
